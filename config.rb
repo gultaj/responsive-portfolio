7c103901dc4e7a0f402cb24cefc77cc990018d48
gultaj_home <yaschick.mail@gmail.com>
2015-01-20 00:21:31 +0300

    sass issues

---
+++ config.rb
@@ -0,0 +1,17 @@
+require 'sass-globbing'
+
+project_path = "./"
+css_dir = "css/"
+sass_dir = "sass/"
+#images_dir = "img"
+#javascripts_dir = "js"
+
+output_style = :expanded 
+#:compact
+
+#relative_assets = true
+
+line_comments = false
+
+
+sourcemap = true
